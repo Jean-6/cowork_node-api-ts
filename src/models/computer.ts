5d6a3c483951f6ebe99cf413208ef4014ad9f4c5
jean-jaures <oka.jeanjaures@sncf.fr>
2024-06-10 11:35:32 +0200

    data modeling

---
+++ src/models/computer.ts
@@ -0,0 +1,12 @@
+export interface Computer {
+
+    brand : string,
+    model : string,
+    numberOfCore : number,
+    frquency : number,
+    RAMCapacity : number,
+    storageCapacity : number,
+    operatingSystem : string,
+
+
+}
