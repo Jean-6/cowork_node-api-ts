118f4143fd18c6e0f55cb06dcd1bf12a8aecd60c
jean-jaures <oka.jeanjaures@sncf.fr>
2024-06-16 22:03:38 +0200

    route connection

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,44 @@
-import express, {Request,Response} from 'express';
+import express, { Router , Request , Response } from 'express';
 
+//Route connection
+import computer from './routes/computer';
+import center from './routes/center';
+import printer from './routes/printer';
+import auth from './routes/auth';
+import user from './routes/user';
 
+import swaggerUi from "swagger-ui-express";
+import * as swaggerDocument from "./swagger.json";
 
 // Create Express server
 const app = express(); // Express instance
-const port = 3000; // Port Number
+const port = process.env.PORT || 3000; // Port Number
 
+
+app.use(express.json);
+
+//Computer
+app.use('/computer',computer);
+//Printer
+app.use ('/printer',printer);
+//Center
+app.use('/center',center);
+//Auth
+app.use ('/auth',auth);
+//User
+app.use ('/user',user);
+
+
+
+
+
+
+
+
+// Documentation
+app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocument));
 
 // Express configuration
-
-
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello from Server : TypeScript + Node.js + Express !');
@@ -18,8 +47,6 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
-
-
 
 // Export Express app
 export default app;
