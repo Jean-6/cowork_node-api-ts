e781d1bb6eea5b23efd0297d613e9ac5d7bd8315
jean-jaures <oka.jeanjaures@sncf.fr>
2024-07-14 13:21:38 +0200

    feature (node server) : configuration

--- src/server.ts
+++ src/server.ts
@@ -1,42 +1,34 @@
-import express, { Router , Request , Response } from 'express';
-
-//Route connection
-import computer from './routes/computer';
-import center from './routes/center';
-import printer from './routes/printer';
-import auth from './routes/auth';
+import express, {json, Request, Response} from 'express';
 import user from './routes/user';
 
 import swaggerUi from "swagger-ui-express";
 import * as swaggerDocument from "./swagger.json";
+import bodyParser from "body-parser";
 
-// Create Express server
-const app = express(); // Express instance
-const port = process.env.PORT || 3000; // Port Number
 
+
+//Express App
+const app = express();
 
 app.use(express.json);
 
-//Computer
-app.use('/computer',computer);
-//Printer
-app.use ('/printer',printer);
-//Center
-app.use('/center',center);
-//Auth
-app.use ('/auth',auth);
-//User
-app.use ('/user',user);
+app.use((re,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin','*'); // To access to API from anywhere origin
+    res.setHeader('Access-Control-Allow-Headers','Origin,X-Requested-With,Content,Accept,Content-Type, Authorization'); // Add headers mentioned to request sent toward API
+    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, PATCH, OPTIONS'); // Allow to send request with this methods
+    next();
+});
+app.use(bodyParser.json())
 
+// API
+app.use ('/api/user', (req, res) => {
 
+    res.send({message: 'create user controller'});
 
-
-
-
-
+});
 
 // Documentation
-app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocument));
+//app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocument));
 
 // Express configuration
 
@@ -44,7 +36,8 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello from Server : TypeScript + Node.js + Express !');
 });
 
-app.listen(port, () => {
+const port = process.env.PORT || 3000; // Port Number
+app.listen(process.env.PORT || 3000, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
 
